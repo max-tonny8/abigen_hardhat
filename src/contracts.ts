931816af574dfe55588c08c70baeab06cd1fe32d
tonny0831 <tonnyjansen0831@gmail.com>
2023-06-04 11:45:17 +0300

    Exited package.json

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,8 +2,8 @@ import { Artifacts } from "hardhat/types";
 
 export async function getContracts(artifacts: Artifacts, basepath: string, extra: string[] = []): Promise<string[]> {
   return (await artifacts.getAllFullyQualifiedNames())
-    .filter(name => name.startsWith(basepath)) // Only files in basepath directory
-    .map(name => name.split(":")[1]) // Get contract name
-    .filter((name, _, names) => names.indexOf(name.slice(1)) === -1) // Only contracts
-    .filter(name => extra.indexOf(name) === -1); // Remove extra contracts
+    .filter(name => name.startsWith(basepath))
+    .map(name => name.split(":")[1])
+    .filter((name, _, names) => names.indexOf(name.slice(1)) === -1)
+    .filter(name => extra.indexOf(name) === -1);
 }
